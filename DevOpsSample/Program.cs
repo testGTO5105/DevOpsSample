e95a70dd61a6277615c207550418553a544efbf8
test_bot <555botggto@gmail.com>
2018-12-15 16:04:19 +0900

    修正

--- DevOpsSample/Program.cs
+++ DevOpsSample/Program.cs
@@ -2,11 +2,11 @@
 
 namespace DevOpsSample
 {
-    class Program
+    static class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hello World!");
         }
     }
 }
